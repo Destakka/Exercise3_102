081a7f95042f5b9e814dd74c78808f8f2eb2cdae
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-25 16:17:16 +0700

    add public bool Search

--- Exercise3_102/Program.cs
+++ Exercise3_102/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,19 @@ namespace Exercise_Linked_List_A
         public CircularList()
         {
             LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true) /*Return true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber) /*If the node is present at the end*/
+                return true;
+            else
+                return (false); /*return false if the node is not found*/
         }
     }
 }
